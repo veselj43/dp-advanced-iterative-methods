1a7b40c28929d4ef87b96ed5c2fcabe99b5e33fd
Jaroslav Veselý <j4vesely@gmail.com>
2017-11-04 22:56:30 +0100

    dev toolbar

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,24 @@
     <div id="advancedIterativeMethods">
         <navbar></navbar>
         <router-view class="__router-view"></router-view>
+        <dev-controls v-if="debugMode"></dev-controls>
     </div>
 </template>
 
 <script>
+import DevControls from './components/devTools/DevControls';
 import Navbar from './components/Navbar';
 
 export default {
     name: 'advancedIterativeMethods',
     components: {
+        DevControls,
         Navbar
+    },
+    data() {
+        return {
+            debugMode: this.$store.state.debugMode
+        }
     }
 }
 </script>
