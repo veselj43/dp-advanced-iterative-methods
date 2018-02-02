81e7fd9ba1f06efcde1d14129cd392982e19115a
Jaroslav Veselý <j4vesely@gmail.com>
2018-02-02 13:56:12 +0100

    dc instead of vue-chartjs
    - for now, new datasets are created for every change (= not optimal solution)

--- src/components/output/Results.vue
+++ src/components/output/Results.vue
@@ -3,14 +3,13 @@
         <h3>Results <small v-if="computingIsProcessingResults">{{computingStatus.text}}</small></h3>
         <img v-if="computingIsProcessingResults" src="/static/img/loading_01.svg" alt="Processing results">
 
-        <div id="lineChart"></div>
+        <div v-show="computingIsRunning || computingIsProcessingResults">
 
-        <!-- <div v-if="computingIsRunning || computingIsProcessingResults">
-
-            <live-line-chart class="chart"
+            <!-- <live-line-chart class="chart"
                 v-bind="liveData.chart"
                 :height="300"
-            ></live-line-chart>
+            ></live-line-chart> -->
+            <div id="liveLineChart"></div>
 
             <table class="table table-bordered table-hover">
                 <tbody>
@@ -27,12 +26,13 @@
 
         </div>
 
-        <div v-else-if="comparingResultsInfo.activeCount > 0">
+        <div v-show="comparingResultsInfo.activeCount > 0 && !(computingIsRunning || computingIsProcessingResults)">
 
-            <multiple-line-chart class="chart"
+            <!-- <multiple-line-chart class="chart"
                 v-bind="dataForMultipleLineChart"
                 :height="300"
-            ></multiple-line-chart>
+            ></multiple-line-chart> -->
+            <div id="multipleLineChart"></div>
 
             <table class="table table-bordered table-hover">
                 <thead>
@@ -57,9 +57,9 @@
 
         </div>
 
-        <div v-else>
+        <div v-show="!(comparingResultsInfo.activeCount > 0 && !(computingIsRunning || computingIsProcessingResults))">
             <p>Compute an instance or check instances to compare from the right panel.</p>
-        </div> -->
+        </div>
 
     </div>
 </template>
@@ -101,8 +101,8 @@ export default {
     },
 
     mounted() {
-        this.compositeChart = dc.compositeChart('#lineChart');
-        this.compositeChart
+        this.multipleLineChart = dc.compositeChart('#multipleLineChart');
+        this.multipleLineChart
             .width(800).height(300)
             .x(d3.scale.linear().domain([0, 100]))
             .y(d3.scale.linear().domain([0, 100]))
@@ -110,12 +110,54 @@ export default {
             .elasticY(true)
             .brushOn(false)
             .transitionDuration(0);
-        this.compositeChart.render();
+        this.multipleLineChart.render();
+
+        this.liveLineChart = dc.compositeChart('#liveLineChart');
+        this.liveLineChart
+            .width(800).height(300)
+            .x(d3.scale.linear().domain([0, 100]))
+            .y(d3.scale.linear().domain([0, 100]))
+            .brushOn(false)
+            .transitionDuration(0);
+        this.liveLineChart.render();
     },
 
     methods: {
-        updateCompositeChart(options) {
-            var compositeChart = this.compositeChart;
+        updateLiveLineChart(options) {
+            var liveLineChart = this.liveLineChart;
+            var liveData = this.liveData;
+
+            if (options && options.init) {
+                liveLineChart
+                    .x(d3.scale.linear().domain([0, liveData.chart.labels.length + 100]))
+                    .y(d3.scale.linear().domain([0, liveData.best + 10]))
+                    .compose([]);
+                liveLineChart.render();
+
+                return;
+            }
+
+            var dim = crossfilter(liveData.chart.values).dimension((d, c) => c);
+            var grp = dim.group().reduceSum((d) => d);
+
+            liveLineChart
+                .dimension(dim)
+                .compose([
+                    dc.lineChart(liveLineChart)
+                        .group(grp)
+                        .colors('red')
+                ]);
+
+            if (options && options.render) {
+                liveLineChart.render();
+            }
+            else {
+                liveLineChart.redraw();
+            }
+        },
+        
+        updateMultipleLineChart(options) {
+            var multipleLineChart = this.multipleLineChart;
             var dataSets = this.dataForMultipleLineChart.dataSets;
 
             if (dataSets.length === 0) return;
@@ -134,23 +176,32 @@ export default {
 
             dataSets = dataSets.map((dataSet, i) => {
                 var grp = dataSet.group().reduceSum((d) => d);
-                return dc.lineChart(compositeChart).group(grp).colors(colors[i % colors.length]);
+                return dc.lineChart(multipleLineChart).group(grp).colors(colors[i % colors.length]);
             });
 
-            compositeChart
+            multipleLineChart
                 .dimension(dim)
                 .compose(dataSets);
 
             if (options && options.render) {
-                compositeChart.render();
+                multipleLineChart.render();
             }
             else {
-                compositeChart.redraw();
+                multipleLineChart.redraw();
             }
         }
     },
 
     watch: {
+        'liveData.chart.values.length'(newValue, oldValue) {
+            if (newValue < oldValue || oldValue === 0) {
+                this.updateLiveLineChart({ init: true });
+            }
+            else {
+                this.updateLiveLineChart();
+            }
+        },
+
         'dataForMultipleLineChart.dataSets.length'(newValue, oldValue) {
             var oldMaxDatasetLength = this.maxDatasetLength;
             this.maxDatasetLength = Math.max(...this.dataForMultipleLineChart.dataSets.map(d => d.data.length));
@@ -159,10 +210,10 @@ export default {
                 oldValue === 0 ||
                 oldMaxDatasetLength < this.maxDatasetLength
             ) {
-                this.updateCompositeChart({ render: true });
+                this.updateMultipleLineChart({ render: true });
             }
             else {
-                this.updateCompositeChart();
+                this.updateMultipleLineChart();
             }
         }
     }
