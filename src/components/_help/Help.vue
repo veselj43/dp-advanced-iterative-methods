a501f5d32797b1569543b8df5fed5647b684cc4a
Adam Kugler <kugleada@fit.cvut.cz>
2018-05-02 12:33:02 +0200

    Added help for ETSP

--- src/components/_help/Help.vue
+++ src/components/_help/Help.vue
@@ -29,10 +29,13 @@
                       <li>
                           <router-link to="#TSP">Travelling salesman</router-link>
                       </li>
+                        <li>
+                        <router-link to="#ETSP">Euclidean travelling salesman</router-link>
+                        </li>
                       <li>
                           <router-link to="#MVC">Minimal vertex cover</router-link>
                       </li>
-                    </router-link>
+                </router-link>
                 </li>
             </ul>
 
@@ -127,6 +130,21 @@
                   <p>*[list of nodes to visit in ascending order]</p>
                   <p>[grapf as an 2D array, space separators, index is vertex, value is the weight of and edge, 0 means no edge]</p>
                   <p>* is there only for "Shortest" type</p>
+            <a href="#ETSP"></a>
+            <h3>Euclidean travelling salesman problem</h3>
+            <p>Variant of travelling salesman problem. Distances between cities aren't given explicitly but they are computed as Euclidean distance from coordinates of cities. Application supports just 2 coordinates.</p>
+            <p>Given <em>n</em> points in <em>R^2</em>, the problem is to find the minimum length tour of the n points starting and ending in the same point.
+                The distance between any two points <em>x</em> and <em>y</em> is defined to be the Euclidean distance between them, i.e., <em>&radic;<span style="border-top: 1px solid #000000">(x_1 - y_1)*(x_1 - y_1) + (x_2 - y_2)*(x_2 - y_2)</span></em>.</p>
+            <h4>Price function</h4>
+            <p>Price function is the length of the cycle/sequence.</p>
+
+            <h4>Generator</h4>
+            <p>Generator generates coordinates uniformly from 0 to given non-negative range.</p>
+            <h4>Instance format</h4>
+            <p>Format is:</p>
+            <p><b>[number of cities]</b> <em>//rest of the line is ignored</em></p>
+            <p>{<b>[x coordinate] [y coordinate]</b>  <em>//rest of the line is ignored</em>}^number of cities</p>
+
                 <a href="#MVC"></a>
                 <h3>Minimum vertex cover</h3>
                 <p>A minimum vertex cover is a vertex cover having the smallest possible number of vertices for a given graph.</p>
