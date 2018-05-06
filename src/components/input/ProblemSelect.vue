7c896f7c4e569f5e03a06405da598fb7e0a92fae
Adam Kugler <kugleada@fit.cvut.cz>
2018-05-06 12:13:19 +0200

    Added tooltip with link to help to problem.

--- src/components/input/ProblemSelect.vue
+++ src/components/input/ProblemSelect.vue
@@ -4,6 +4,7 @@
 
             <div class="form-group">
                 <label for="problemKey">Problem</label>
+                <router-link to="/help/Problems" class="form-tooltip" v-tooltip.right="'Problem to be solved by this algorithm. To learn more about problems and input formats see help.'"><i class="fas fa-question-circle"></i></router-link>
                 <select class="form-control" id="problemKey" v-model="problemKey">
                     <option v-for="(problem, index) in problemEnum" :key="index" :value="index">{{problem.text}}</option>
                 </select>
